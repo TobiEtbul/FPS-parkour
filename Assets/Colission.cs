4f77d01182af8947cc89c60ebeacad435f187cb0
TobiEtbul <etbultobias@gmail.com>
2025-05-29 12:07:13 -0300

    bvfrbv

---
+++ Assets/Colission.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colission : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
