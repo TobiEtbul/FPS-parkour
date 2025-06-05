e0fb754eefd0fe7e9ca82cc0b84298bc6596d5db
TobiEtbul <etbultobias@gmail.com>
2025-06-05 12:14:01 -0300

    eov

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void UpdateMoney (float moneyToAdd)
+    {
+        if(money + moneyToAdd <0){
+            Debug.log ("plata insuficiente");
+            return;
+        }
+        money+= moneyToAdd;
+    }
+}
--- Assets/Prefabs/Mercancia.cs
+++ Assets/Prefabs/Mercancia.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Mercancia : MonoBehaviour
 {
+    public float cost;
+    public MoneyManager moneyManager;
+    void start ()
+    {
+        moneyManager = FindObjectOfType<moneyManager>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "player")
         {
+            moneyManager.UpdateMoney(cost);
             Destroy(gameObject);
         }
     }
